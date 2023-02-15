8ee4f09946bd7140ec2c4dc97f75948a51e0f0c6
Петров Никита <Petovn.xz@mail.ru>
2023-02-15 11:02:26 +0300

    add bricks. add bonus

---
+++ Assets/Scripts/BonusScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class BlockScript:MonoBehaviour
+    {
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+           Destroy(gameObject);
+        }
+    }
+}
